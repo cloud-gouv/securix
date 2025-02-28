c2685d29143fcb2458f4c9de059dc8545aad7c3c
Elias Coppens <elias.coppens@numerique.gouv.fr>
2025-02-28 15:56:38 +0100

    feat: add git commit hooks
    Signed-off-by: Elias Coppens <elias.coppens@numerique.gouv.fr>

--- default.nix
+++ default.nix
@@ -10,14 +10,48 @@
 , edition ? "unbranded"
 }:
 let
+  git-hooks = import sources.git-hooks;
+
   inherit (pkgs) lib;
+
+  git-checks = git-hooks.run {
+    src = ./.;
+
+    hooks = {
+      statix = {
+        enable = true;
+        stages = [ "pre-push" ];
+        settings.ignore = [
+          "**/npins"
+        ];
+      };
+
+      nixfmt-rfc-style = {
+        enable = true;
+        stages = [ "pre-push" ];
+        package = pkgs.nixfmt-rfc-style;
+        args = [ "-s" ];
+      };
+
+      reuse = {
+        enable = true;
+        stages = [ "pre-push" ];
+        package = pkgs.reuse;
+      };
+    };
+  };
 in
 {
   lib = import ./lib { inherit pkgs lib edition defaultTags sources; };
   modules = ./modules;
   shell = pkgs.mkShell {
     packages = [
-      pkgs.reuse
+      pkgs.npins
+      (pkgs.callPackage "${sources.agenix}/pkgs/agenix.nix" { })
+    ] ++ git-checks.enabledPackages;
+
+    shellHook = lib.concatStringsSep "\n" [
+      git-checks.shellHook
     ];
   };
 }
