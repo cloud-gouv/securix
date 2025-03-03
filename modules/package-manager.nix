14488715606f6ce1cc071fa264a50a432ac8839f
Elias Coppens <elias.coppens@numerique.gouv.fr>
2025-03-03 11:38:00 +0100

    feat: use proxies in nix daemon
    Signed-off-by: Elias Coppens <elias.coppens@numerique.gouv.fr>

--- modules/package-manager.nix
+++ modules/package-manager.nix
@@ -1,12 +1,34 @@
 # SPDX-FileCopyrightText: 2025 Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
+# SPDX-FileContributor: Elias Coppens <elias.coppens@numerique.gouv.fr>
 #
 # SPDX-License-Identifier: MIT
 
-{ pkgs, ... }:
 {
-  nix.package = pkgs.lix;
-  nix.nixPath = [
-    # Always point to the authorized sources.
-    "nixpkgs=${pkgs.path}"
-  ];
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  proxyCfg = config.securix.http-proxy;
+in
+{
+  config = {
+    nix =
+      {
+        package = pkgs.lix;
+        nixPath = [
+          # Always point to the authorized sources.
+          "nixpkgs=${pkgs.path}"
+        ];
+      }
+      // (lib.optionalAttrs proxyCfg.enable {
+        envVars = {
+          http_proxy = proxyCfg.usedProxyAddress;
+          https_proxy = proxyCfg.usedProxyAddress;
+          all_proxy = proxyCfg.usedProxyAddress;
+          no_proxy = lib.concatStringsSep "," proxyCfg.exceptions;
+        };
+      });
+  };
 }
