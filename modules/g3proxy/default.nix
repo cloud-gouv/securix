a63423ac0c5e61d058835589feecd330320a33d5
Elias Coppens <elias.coppens@numerique.gouv.fr>
2025-03-04 09:25:03 +0100

    feat(modules): init g3proxy module
    Co-Authored-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
    Signed-off-by: Elias Coppens <elias.coppens@numerique.gouv.fr>

---
+++ modules/g3proxy/default.nix
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2025 Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
+#
+# SPDX-License-Identifier: MIT
+
+{ imports = [ ./module.nix ]; }
---
+++ modules/g3proxy/module.nix
@@ -0,0 +1,111 @@
+# SPDX-FileCopyrightText: 2025 Elias Coppens <elias.coppens@numerique.gouv.fr>
+# SPDX-FileContributor: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
+#
+# SPDX-License-Identifier: MIT
+
+# Remove this when https://github.com/NixOS/nixpkgs/pull/378059 is merged and we upgraded nixpkgs.
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.services.g3proxy;
+
+  inherit (lib)
+    mkEnableOption
+    mkOption
+    mkIf
+    literalExpression
+    ;
+
+  settingsFormat = pkgs.formats.yaml { };
+
+  package = pkgs.callPackage ./package.nix { };
+in
+{
+  options.services.g3proxy = {
+    enable = mkEnableOption "g3proxy";
+
+    settings = mkOption {
+      type = settingsFormat.type;
+      default = { };
+      example = literalExpression ''
+        {
+          server = [{
+            name = "test";
+            escaper = "default";
+            type = "socks_proxy";
+            listen = {
+              address = "[::]:10086";
+            };
+          }];
+        }
+      '';
+      description = ''
+        Settings of g3proxy.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.g3proxy.settings.log = lib.mkDefault "stdout";
+    systemd.services.g3proxy = {
+      description = "g3proxy server";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+
+      serviceConfig = {
+        ExecStart =
+          let
+            g3proxy-yaml = settingsFormat.generate "g3proxy.yaml" cfg.settings;
+          in
+          "${package}/bin/g3proxy --config-file ${g3proxy-yaml} --control-dir $RUNTIME_DIRECTORY -s -G default";
+
+        ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+        ExecStop = "${package}/bin/g3proxy-ctl --control-dir $RUNTIME_DIRECTORY -G default -p $MAINPID offline";
+
+        WorkingDirectory = "/var/lib/g3proxy";
+        StateDirectory = "g3proxy";
+        RuntimeDirectory = "g3proxy";
+        RuntimeDirectoryPreserve = true;
+
+        SuccessExitStatus = "SIGQUIT";
+        RestartPreventExitStatus = 255;
+        TimeoutStartSec = 10;
+        LimitNOFILE = 10485760;
+        Restart = "on-failure";
+        Type = "simple";
+        # Harden entirely the unit until a correct audit is performed on the code.
+        DynamicUser = true;
+
+        # RuntimeDirectoryMode = "0755";
+        PrivateTmp = true;
+        DevicePolicy = "closed";
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        PrivateUsers = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectControlGroups = true;
+        ProtectSystem = "strict";
+        ProcSubset = "pid";
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RemoveIPC = true;
+        SystemCallArchitectures = "native";
+        UMask = "0077";
+        RestrictAddressFamilies = [
+          "AF_UNIX"
+          "AF_INET"
+          "AF_INET6"
+        ];
+        RestrictSUIDSGID = true;
+      };
+    };
+  };
+}
