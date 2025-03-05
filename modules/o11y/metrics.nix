a2fd4e67b29e8c2b43b59f04c9af1089e9dabff1
Elias Coppens <elias.coppens@numerique.gouv.fr>
2025-03-05 17:14:09 +0100

    feat(modules/o11y): enable some prometheus exporters
    Co-Authored-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
    Signed-off-by: Elias Coppens <elias.coppens@numerique.gouv.fr>

--- modules/o11y/metrics.nix
+++ modules/o11y/metrics.nix
@@ -10,6 +10,8 @@ let
     mkOption
     types
     mkIf
+    mapAttrsToList
+    filterAttrs
     ;
 in
 {
@@ -19,9 +21,55 @@ in
   };
 
   config = mkIf cfg.enable {
+    # This will ship node_exporter-like metrics to the remote servers.
+    services.prometheus.exporters.node = {
+      enable = true;
+      enabledCollectors = [
+        "systemd"
+        "processes"
+      ];
+    };
+
+    # This will ship electricity metrics to the remote servers.
+    services.prometheus.exporters.scaphandre = {
+      enable = true;
+    };
+
     services.vmagent = {
       enable = true;
       remoteWrite.url = cfg.serverUrl;
+      prometheusConfig = {
+        scrape_configs =
+          mapAttrsToList
+            (job_name: cfg: {
+              inherit job_name;
+              static_configs = [ { targets = [ "127.0.0.1:${builtins.toString cfg.port}" ]; } ];
+              metrics_path = if job_name == "scaphandre" then "//metrics" else "/metrics";
+            })
+            (
+              filterAttrs (
+                name: cfg:
+                # these are not working Prometheus Exporters
+                !(builtins.elem name [
+                  "assertions"
+                  "warnings"
+                  "blackbox"
+                  "unifi-poller"
+                  "domain"
+                  "minio"
+                  "idrac"
+                  "pve"
+                  "tor"
+                ])
+                && cfg.enable
+              ) config.services.prometheus.exporters
+            );
+        global = {
+          scrape_interval = "15s";
+          external_labels.hostname = config.networking.hostName;
+          # TODO: devrait-on-mettre un label supplémentaire d'appartenance d'équipe au niveau des time series exportés?
+        };
+      };
     };
   };
 }
