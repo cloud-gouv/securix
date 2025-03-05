36562025ff23439f945ed3f1b0ef71ec6d97f985
Elias Coppens <elias.coppens@numerique.gouv.fr>
2025-03-05 17:16:54 +0100

    feat(vpn/netbird): init
    Co-Authored-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
    Signed-off-by: Elias Coppens <elias.coppens@numerique.gouv.fr>

---
+++ modules/vpn/netbird/default.nix
@@ -0,0 +1,67 @@
+# SPDX-FileCopyrightText: 2025 Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
+#
+# SPDX-License-Identifier: MIT
+
+{
+  vpnProfiles,
+  operators,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.securix.vpn.netbird;
+  inherit (lib)
+    mkIf
+    mkEnableOption
+    listToAttrs
+    concatMapAttrs
+    filter
+    ;
+  selectNetbirdVpns = list: filter (vpnName: vpnProfiles.${vpnName}.type == "netbird") list;
+in
+{
+  options.securix.vpn.netbird = {
+    enable = mkEnableOption "the Netbird VPN subsystem";
+
+    enablePostQuantumCryptography = mkEnableOption "the post-quantum cryptography in WireGuard with Rosenpass";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      netbird
+      netbird-ui
+    ];
+
+    services.netbird = {
+      enable = lib.mkForce false;
+      clients = concatMapAttrs (
+        operatorName:
+        { username, allowedVPNs, ... }:
+        listToAttrs (
+          map (
+            vpnName:
+            let
+              vpnProfile = vpnProfiles.${vpnName};
+            in
+            {
+              name = "${operatorName}-${vpnName}";
+              value = {
+                ui.enable = true;
+                interface = "nb-${vpnName}";
+                port = 51820;
+                hardened = false;
+                config = {
+                  ManagementURL = vpnProfile.management-url;
+                  AdminURL = vpnProfile.admin-url;
+                  RosenpassEnabled = cfg.enablePostQuantumCryptography;
+                };
+              };
+            }
+          ) (selectNetbirdVpns allowedVPNs)
+        )
+      ) operators;
+    };
+  };
+}
