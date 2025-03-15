7b3a0c10f5a82623a9d62ab5081d3167cddccd61
soyouzpanda <soyouzpanda@soyouzpanda.fr>
2025-03-15 19:37:57 +0100

    fix: fix example

---
+++ examples/basic/inventory/test-machine.nix
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2025 soyouzpanda <soyouzpanda@soyouzpanda.fr>
+#
+# SPDX-License-Identifier: MIT
+
+{
+  securix.self = {
+    mainDisk = "/dev/sda";
+    defaultOperator = "testuser";
+    inventoryId = 123456;
+    hardwareSKU = "x280";
+    developer = true;
+    team = "test-team";
+  };
+}
---
+++ examples/basic/operators.nix
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2025 soyouzpanda <soyouzpanda@soyouzpanda.fr>
+#
+# SPDX-License-Identifier: MIT
+
+_: {
+  securix.users.users.testuser = {
+    username = "testuser";
+    email = "testuser@example.com";
+    hashedPassword = "$y$j9T$Vklt7OGx1hXyM4AkGt3JB1$n.eIcU672OYv3RgU2XH1XResKnehqxgrTkIozzHRUp8";
+    teams = [ "test-team" ];
+    developer = true;
+  };
+}
