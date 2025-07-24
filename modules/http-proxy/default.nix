9811f03b9df3bc6b19904a5c4f5f52dd0502824a
Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
2025-07-24 02:10:50 +0200

    feat(http-proxy): dynamic proxy switching
    * makes 2 endpoints: one for the default entrypoint, one for the
    internal fwd proxy
    
    * add a new program in $PATH to select via a TUI a proxy origin and
    publish it to g3proxy via RPC
    
    * expose downstreams so that users can configure multiple fallbacks
    
    Signed-off-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>

--- modules/http-proxy/default.nix
+++ modules/http-proxy/default.nix
@@ -3,7 +3,12 @@
 #
 # SPDX-License-Identifier: MIT
 
-{ lib, config, ... }@args:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}@args:
 let
   inherit (lib)
     mkEnableOption
@@ -13,7 +18,9 @@ let
     types
     concatStringsSep
     ;
+  json = pkgs.formats.json { };
   cfg = config.securix.http-proxy;
+  proxyConfigFile = json.generate "proxies.json" cfg.downstreams;
 
   securix-lib = import ../lib/utils.nix args;
 
@@ -34,6 +41,18 @@ in
   options.securix.http-proxy = {
     enable = mkEnableOption "configure un proxy HTTP client SOCKS5 globalement";
 
+    downstreams = mkOption {
+      type = types.attrsOf types.str;
+      examples = [
+        {
+          par_a = "1.2.3.4:8080";
+          par_b = "1.5.6.8:8080";
+          rbx_a = "1.8.9.2:9091";
+        }
+      ];
+      description = "Ensembles des proxies descendants";
+    };
+
     exceptions = mkOption {
       type = types.listOf types.str;
       # Exclude localhost and its IP variants for now.
@@ -53,6 +72,26 @@ in
         noProxy = concatStringsSep "," cfg.exceptions;
       };
 
+      environment.etc."proxy-switcher/proxies.json".file = proxyConfigFile;
+      environment.systemPackages = [
+        # TODO: add jq, whiptail in the PATH of that script.
+        (pkgs.writeShellScriptBin "proxy-switcher" ./proxy-switcher.sh)
+      ];
+
+      # Static allocation of UID/GID for g3proxy. 
+      # Necessary to perform the nftables rule targeting.
+      ids.uids.g3proxy = 398;
+      ids.gids.g3proxy = 398;
+
+      users.users.g3proxy = {
+        isSystemUser = true;
+        uid = config.ids.uids.g3proxy;
+      };
+
+      users.groups.g3proxy = {
+        gid = config.ids.gids.g3proxy;
+      };
+
       services.g3proxy = {
         enable = true;
         settings = {
@@ -60,29 +99,44 @@ in
             {
               name = "default";
               type = "c-ares";
-              # CloudFlare… Not optimal.
-              # TODO: does the MTE have a public DNS?
-              # https://www.joindns4.eu/
-              # 86.54.11.100 (unfiltered resolver)
-              server = "86.54.11.100";
-              # By default, always use secure resolution. Never leak any metadata to upstream DNS servers.
-              # encryption = "dns-over-https";
             }
           ];
 
           escaper = [
+            # Link the default escaper to the special internal HTTP proxy.
             {
               name = "default";
               type = "direct_fixed";
               no_ipv6 = false;
               resolver = "default";
             }
+
+            # This is the dynamic escaper where a couple of proxies
+            # can be set via the proxy-switcher dynamically over Cap'n'Proto RPC.
+            {
+              name = "dynamic";
+              type = "proxy_float";
+              source = "passive";
+            }
           ];
 
           server = [
+            # This is the fallback local forward proxy.
+            # Used for network locations with no forward proxy.
+            {
+              name = "local_securix";
+              escaper = "default";
+              # TODO: intelli_proxy here.
+              type = "http_proxy";
+              listen.address = "127.0.0.1:8081";
+              tls_client = { };
+            }
+
+            # This is the entrypoint of all proxy requests.
             {
               name = "securix";
-              escaper = "default";
+              escaper = "dynamic";
+              # TODO: intelli_proxy here.
               type = "http_proxy";
               listen.address = "127.0.0.1:8080";
               tls_client = { };
