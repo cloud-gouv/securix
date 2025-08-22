aa2837bb32455d39ff2f3e00f8893031dbb2dffb
Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
2025-08-22 15:52:26 +0200

    fix(modules/g3proxy): disable upstream g3proxy module
    g3proxy has been upstreamed as a module in NixOS but its module is not
    exactly what we need because it's way too much hardened for now.
    
    Signed-off-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>

--- modules/g3proxy/default.nix
+++ modules/g3proxy/default.nix
@@ -1,5 +1,7 @@
 # SPDX-FileCopyrightText: 2025 Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
 #
 # SPDX-License-Identifier: MIT
-
-{ imports = [ ./module.nix ]; }
+{
+  imports = [ ./module.nix ];
+  disabledModules = [ "services/networking/g3proxy.nix" ];
+}
