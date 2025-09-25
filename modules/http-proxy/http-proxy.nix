dd45598e5a15e6a0ed865234ca21a8d2fa137898
plegrand-dinum <pauline.legrand@numerique.gouv.fr>
2025-09-25 08:38:54 +0200

    chore(current-proxy): add config test

--- modules/http-proxy/http-proxy.nix
+++ modules/http-proxy/http-proxy.nix
@@ -167,6 +167,7 @@ in
               name = "dynamic";
               type = "proxy_float";
               source.type = "passive";
+              cache_file = "/run/proxy/current.json";
             }
           ];
 
