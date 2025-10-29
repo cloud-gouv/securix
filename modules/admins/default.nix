cloud-gouv/securix

commit 772aa23f9e0fb07752b3b48881637475be39036a

modules/self: render U2F tokens uniform across admins and users
This renames tokens into u2f_keys for admins as well.

Signed-off-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>

modules/admins/default.nix
@@ -23,9 +23,14 @@ let
           description = "PAM name for this account";
         };
 
-        tokens = mkOption {
+        u2f_keys = mkOption {
           type = types.listOf types.str;
-          description = "List of PAM U2F tokens that are allowed to connect to this account";
+          description = ''
+            List of PAM U2F keys that are allowed to connect to this account
+
+            NOTE: you have to use pamu2cfg with the same parameters for appid
+            and origin as the one described in `config.securix.pam.u2f`.
+          '';
         };
       };
     };

modules/self.nix
@@ -97,6 +97,16 @@ in
         description = "Mot de passe hachée en ycrypt pour la session utilisateur.";
       };
 
+      u2f_keys = mkOption {
+        type = types.listOf types.str;
+        default = [ ];
+        description = ''
+          Liste de clefs U2F générés par `pamu2cfg`
+          NOTE: Il faut passer les bons paramètres appid et origin à pamu2cfg si on veut que la clef soit reconnue.
+          Ces paramètres sont documentés dans `config.securix.pam.u2f`.
+        '';
+      };
+
       defaultLoginShell = mkOption {
         type = types.package;
         default = pkgs.bashInteractive;
@@ -259,6 +269,7 @@ in
         Le mode développeur est activé pour ${cfg.user.email}, cette image n'est pas conforme aux règles de l'ANSSI.
       '';
       users.users.${cfg.user.username}.shell = cfg.user.defaultLoginShell;
+      securix.pam.u2f.keys.${cfg.user.username} = cfg.user.u2f_keys;
     })
   ];
 }

commit 93516bb0bc60f558213f204bf1bd3b20a5d79496

fix(modules/self): use `u2f_keys` rather than `tokens`
Fixes 772aa23f introduced mistake.

Signed-off-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>

modules/admins/default.nix
@@ -42,7 +42,7 @@ let
       isNormalUser = true;
       extraGroups = [ "wheel" ];
     };
-  mkAdminU2F = _: { name, tokens, ... }: nameValuePair name tokens;
+  mkAdminU2F = _: { name, u2f_keys, ... }: nameValuePair name u2f_keys;
 in
 {
   options.securix.admins = {