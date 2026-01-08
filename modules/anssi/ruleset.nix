ae11fad6da7c72a7e93b05f3de334debcc0f1b5d
Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
2026-01-08 18:14:12 +0100

    modules/anssi: write down todolist of the rest
    Signed-off-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>

--- modules/anssi/ruleset.nix
+++ modules/anssi/ruleset.nix
@@ -9,4 +9,15 @@ in
 loadRules [
   ./preboot.nix
   ./kernel-options.nix
+  # ./kernel.nix
+  # ./vfs.nix
+  # ./users.nix
+  # ./apparmor.nix
+  # ./selinux.nix
+  # ./journaling.nix
+  # ./runtime-minimization.nix
+  # ./hardening.nix
+  # ./nss-hardening.nix
+  # ./integrity.nix
+  # ./mta.nix
 ]
