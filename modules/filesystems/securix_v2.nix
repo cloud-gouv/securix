679801cb17cbb74364514e0329133f75f4770dcc
Julien DAUPHANT <julien.dauphant@numerique.gouv.fr>
2026-01-12 22:10:53 +0100

    format: nixfmt

--- modules/filesystems/securix_v2.nix
+++ modules/filesystems/securix_v2.nix
@@ -11,67 +11,78 @@ let
   disk = config.securix.self.mainDisk;
 in
 {
-  config = mkIf (cfg.enable && lib.lists.elem cfg.layout ["office_v1" "securix_v2"]) {
-    disko.devices = {
-      disk = {
-        ${disk} = {
-          device = "${disk}";
-          type = "disk";
-          content = {
-            type = "gpt";
-            partitions = {
-              ESP = {
-                end = "1G";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                };
-              };
-              Recovery = {
-                end = "+2G";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/recovery";
-                };
-              };
-              luks = {
-                size = "100%";
-                content = {
-                  type = "luks";
-                  name = "croot";
-                  settings = {
-                    allowDiscards = true;
+  config =
+    mkIf
+      (
+        cfg.enable
+        && lib.lists.elem cfg.layout [
+          "office_v1"
+          "securix_v2"
+        ]
+      )
+      {
+        disko.devices = {
+          disk = {
+            ${disk} = {
+              device = "${disk}";
+              type = "disk";
+              content = {
+                type = "gpt";
+                partitions = {
+                  ESP = {
+                    end = "1G";
+                    type = "EF00";
+                    content = {
+                      type = "filesystem";
+                      format = "vfat";
+                      mountpoint = "/boot";
+                    };
                   };
-                  enrollFido2 = true;
-                  enrollRecovery = true;
-                  content = {
-                    type = "btrfs";
-                    mountpoint = "/";
-                    subvolumes = {
-                      "/home" = {
-                        mountpoint = "/home";
-                        mountOptions = [
-                          "compress=zstd"
-                          "noatime"
-                        ];
+                  Recovery = {
+                    end = "+2G";
+                    type = "EF00";
+                    content = {
+                      type = "filesystem";
+                      format = "vfat";
+                      mountpoint = "/recovery";
+                    };
+                  };
+                  luks = {
+                    size = "100%";
+                    content = {
+                      type = "luks";
+                      name = "croot";
+                      settings = {
+                        allowDiscards = true;
                       };
-                      "/var" = {
-                        mountpoint = "/var";
-                        mountOptions = [
-                          "compress=zstd"
-                          "noatime"
-                        ];
-                      };
-                      "/nix" = {
-                        mountpoint = "/nix";
-                        mountOptions = [
-                          "compress=zstd"
-                          "noatime"
-                        ];
+                      enrollFido2 = true;
+                      enrollRecovery = true;
+                      content = {
+                        type = "btrfs";
+                        mountpoint = "/";
+                        subvolumes = {
+                          "/home" = {
+                            mountpoint = "/home";
+                            mountOptions = [
+                              "compress=zstd"
+                              "noatime"
+                            ];
+                          };
+                          "/var" = {
+                            mountpoint = "/var";
+                            mountOptions = [
+                              "compress=zstd"
+                              "noatime"
+                            ];
+                          };
+                          "/nix" = {
+                            mountpoint = "/nix";
+                            mountOptions = [
+                              "compress=zstd"
+                              "noatime"
+                            ];
+                          };
+                        };
                       };
                     };
                   };
@@ -81,6 +92,4 @@ in
           };
         };
       };
-    };
-  };
 }
