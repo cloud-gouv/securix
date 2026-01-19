fd73539c55b1facb09055d7b5c0a5d3008d4e499
Julien DAUPHANT (DINUM) <julien.dauphant@numerique.gouv.fr>
2026-01-19 19:46:52 +0100

    chore: Add noexec to home

--- modules/filesystems/securix_v1.nix
+++ modules/filesystems/securix_v1.nix
@@ -53,6 +53,7 @@ in
                         mountOptions = [
                           "compress=zstd"
                           "noatime"
+                          "noexec"
                         ];
                       };
                       "/nix" = {
--- modules/filesystems/securix_v2.nix
+++ modules/filesystems/securix_v2.nix
@@ -66,6 +66,7 @@ in
                             mountOptions = [
                               "compress=zstd"
                               "noatime"
+                              "noexec"
                             ];
                           };
                           "/var" = {
