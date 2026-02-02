500e4f93a214d5880865b48890f8fe635702842c
swallimann-dinum <steven.wallimann@numerique.gouv.fr>
2026-02-02 17:54:10 +0100

    fix: Remove noexec (#95)

--- modules/filesystems/securix_v2.nix
+++ modules/filesystems/securix_v2.nix
@@ -66,7 +66,6 @@ in
                             mountOptions = [
                               "compress=zstd"
                               "noatime"
-                              "noexec"
                               "nosuid"
                               "nodev"
                             ];
