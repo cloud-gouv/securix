319c32a84bab20e5698953114278eafaacc93289
Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
2026-03-28 00:52:02 +0100

    feat: integrate Portail
    Provide Portail as a new experimental (non-default) option for HTTP
    proxies.
    
    Signed-off-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>

---
+++ modules/http-proxy/portail/default.nix
@@ -0,0 +1,44 @@
+# SPDX-FileCopyrightText: 2026 Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
+#
+# SPDX-License-Identifier: MIT
+
+{ lib, config, ... }:
+let
+  cfg = config.securix.http-proxy;
+  inherit (lib) mkIf concatStringsSep mapAttrs;
+  localProxyStream = "127.0.0.1:8080";
+in
+{
+  config = mkIf (cfg.enable && cfg.implementation == "portail") {
+    networking.proxy = {
+      default = "http://${localProxyStream}";
+      noProxy = concatStringsSep "," cfg.exceptions;
+    };
+
+    # This group is allowed to update any dynamic backend (if there's any).
+    users.groups.portail-admins = { };
+    services.portail = {
+      enable = true;
+      # First request optimization, this avoids the first request to wait for some time needlessly as Portail is pretty fast to boot.
+      enableAtBoot = true;
+      proxyListenStream = localProxyStream;
+
+      acl.filter.rules."99-default" = ''
+        policy default {
+          action allow
+        }
+      '';
+
+      settings = {
+        # By default, we use ourselves to exit.
+        backends = mapAttrs (name: target-address: { inherit target-address; }) cfg.downstreams;
+        rpc = {
+          trusted-groups = [ "operator" ];
+          admin-groups = [ "portail-admins" ];
+        };
+      };
+    };
+
+    systemd.services.portail.aliases = [ "http-proxy.service" ];
+  };
+}
