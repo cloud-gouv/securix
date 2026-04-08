6b124f3591d1e4ba3695f975fa6d17f992a0cf9f
Antoine Eiche <lewo@abesis.fr>
2026-04-08 16:12:16 +0200

    Add option securix.tools.enable
    This is enable by default but this allows users to avoid adding all
    these packages in their environement.
    
    Since these packages are added to the systemPackages list, it was not
    possible to remove all these packages without purging the whole list,
    which can be filled by some others NixOS modules as well.

--- modules/tools/default.nix
+++ modules/tools/default.nix
@@ -2,87 +2,100 @@
 #
 # SPDX-License-Identifier: MIT
 
-{ pkgs, ... }:
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.securix.tools;
+  inherit (lib) mkIf mkEnableOption types;
+in
+{
+  options.securix.tools.enable = mkEnableOption "default Securix tools" // {
+    default = true;
+  };
   imports = [ ./firefox.nix ];
+  config = mkIf cfg.enable {
+    programs.mtr.enable = true;
 
-  programs.mtr.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    # Text editors
-    vscodium
-    vim
-    # TPM2
-    tpm2-tools
-    # A TPM-backed agent for SSH keys
-    ssh-tpm-agent
-    # VNC remoting
-    tigervnc
-    # Some good terminals.
-    alacritty
-    # Uncomment when NixOS 25.05 is used.
-    # ghostty
-    kitty
-    tmux # Multiplexer
-    screen # Multiplexer
-    # Scripting
-    python3
-    gum # TUI scripting
-    # PKI
-    certstrap # Certificate bootstrap for CAs
-    openssl # Generic purpose certificate tooling
-    step-ca # CA tooling
-    opensc # PKCS#11 tooling
-    # Misc
-    termdown # Time counter
-    fd # `find` alternative.
-    ripgrep # super fast `grep`
-    ripgrep-all # multi-format fast `grep`
-    pwgen # Password generator.
-    rofi-rbw-wayland # Rofi menu for rbw.
-    tree # Tree display
-    gnupg # PGP
-    connect # for using ssh with a proxy
-    jq # Lightweight and flexible command-line JSON processor
-    yq-go # Same as jq but for YAML
-    # Git, the full tooling.
-    gitFull
-    git-lfs
-    git-absorb
-    git-gr
-    lazygit
-    jujutsu
-    # Serial console work.
-    minicom
-    picocom
-    # To send files securly to another endpoint.
-    magic-wormhole-rs
-    # iPXE / PXE operations
-    pixiecore
-    # Unzipping
-    unzip
-    # To calculate things
-    libqalculate
-    # Troubleshooting
-    iperf3
-    tcpdump
-    dnsutils
-    conntrack-tools
-    pwru # Packet, where are you? - eBPF tooling
-    strace
-    gdb
-    # Network calculators
-    sipcalc
-    ipv6calc
-    # D-Bus debugging
-    d-spy
-    # Offline documentation
-    #linux-manual
-    glibcInfo
-    man-pages
-    man-pages-posix
-    # Browser
-    firefox
-    qrencode
-  ];
+    environment.systemPackages = with pkgs; [
+      # Text editors
+      vscodium
+      vim
+      # TPM2
+      tpm2-tools
+      # A TPM-backed agent for SSH keys
+      ssh-tpm-agent
+      # VNC remoting
+      tigervnc
+      # Some good terminals.
+      alacritty
+      # Uncomment when NixOS 25.05 is used.
+      # ghostty
+      kitty
+      tmux # Multiplexer
+      screen # Multiplexer
+      # Scripting
+      python3
+      gum # TUI scripting
+      # PKI
+      certstrap # Certificate bootstrap for CAs
+      openssl # Generic purpose certificate tooling
+      step-ca # CA tooling
+      opensc # PKCS#11 tooling
+      # Misc
+      termdown # Time counter
+      fd # `find` alternative.
+      ripgrep # super fast `grep`
+      ripgrep-all # multi-format fast `grep`
+      pwgen # Password generator.
+      rofi-rbw-wayland # Rofi menu for rbw.
+      tree # Tree display
+      gnupg # PGP
+      connect # for using ssh with a proxy
+      jq # Lightweight and flexible command-line JSON processor
+      yq-go # Same as jq but for YAML
+      # Git, the full tooling.
+      gitFull
+      git-lfs
+      git-absorb
+      git-gr
+      lazygit
+      jujutsu
+      # Serial console work.
+      minicom
+      picocom
+      # To send files securly to another endpoint.
+      magic-wormhole-rs
+      # iPXE / PXE operations
+      pixiecore
+      # Unzipping
+      unzip
+      # To calculate things
+      libqalculate
+      # Troubleshooting
+      iperf3
+      tcpdump
+      dnsutils
+      conntrack-tools
+      pwru # Packet, where are you? - eBPF tooling
+      strace
+      gdb
+      # Network calculators
+      sipcalc
+      ipv6calc
+      # D-Bus debugging
+      d-spy
+      # Offline documentation
+      #linux-manual
+      glibcInfo
+      man-pages
+      man-pages-posix
+      # Browser
+      firefox
+      qrencode
+    ];
+  };
 }
