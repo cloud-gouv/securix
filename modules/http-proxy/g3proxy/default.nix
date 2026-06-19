b5340dd6001646ad3ce9eae41bcf57ff6f5ba1b7
Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>
2026-06-19 18:21:21 +0200

    http-proxy/automatic: support dynamically defined proxies
    Sometimes, the remote information is known quite late in the runtime,
    e.g. after a decryption process of some secrets.
    
    To allow for this usecase, we define late bound proxies with a mechanism
    to update them, it is really meant for Portail. g3proxy doesn't support
    that as easily (except by exploiting the same RPC mechanism).
    
    Signed-off-by: Ryan Lahfa <ryan.lahfa.ext@numerique.gouv.fr>

--- modules/http-proxy/g3proxy/default.nix
+++ modules/http-proxy/g3proxy/default.nix
@@ -15,6 +15,7 @@ let
     concatStringsSep
     tail
     splitString
+    mapAttrsToList
     ;
   json = pkgs.formats.json { };
   cfg = config.securix.http-proxy;
@@ -22,6 +23,11 @@ let
 in
 {
   config = mkIf (cfg.enable && cfg.implementation == "g3proxy") {
+    assertions = mapAttrsToList (name: { definition, ... }: {
+      assertion = definition == "dynamic";
+      message = "Upstream proxy ${name} is marked as a dynamically defined proxy. The g3proxy implementation do not support dynamically defined proxies.";
+    }) cfg.proxies;
+
     networking.proxy = {
       default = "http://127.0.0.1:8080";
       noProxy = concatStringsSep "," cfg.exceptions;
