d1edd2b8d9d2ebc9a3e8245540a98940a7e72649
Xavier Maso <xavier.maso@beta.gouv.fr>
2026-07-23 16:58:01 +0200

    Expose tools through a module
    To allow users to be able to pick what they want to use.
    Disabled by default to reduce the supply-chain to a minimum
    out-of-the-box.

--- tests/default.nix
+++ tests/default.nix
@@ -1,4 +1,5 @@
 # SPDX-FileCopyrightText: 2025 Antoine Eiche <aei.ext@hackcyom.com>
+# SPDX-FileContributor: 2026 Xavier Maso <xavier.maso@beta.gouv.fr>
 #
 # SPDX-License-Identifier: MIT
 
@@ -7,4 +8,5 @@
   anssi-minimal = import ./anssi-minimal.nix { inherit pkgs libSecurix; };
   idempotent-autoinstall = import ./idempotent-autoinstall.nix { inherit pkgs libSecurix; };
   portail = import ./portail.nix { inherit pkgs libSecurix; };
+  tools = import ./tools.nix { inherit pkgs libSecurix; };
 }
