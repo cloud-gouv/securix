f7e8d3b2eca5dae1af815ebf1961484f4937d803
Julien DAUPHANT (DINUM) <julien.dauphant@numerique.gouv.fr>
2026-08-26 17:02:51 +0200

    config: Disable PasswordAuthentication for sshd

--- modules/superadmins/default.nix
+++ modules/superadmins/default.nix
@@ -23,6 +23,7 @@ in
   };
   config = mkIf cfg.enable {
     services.openssh.enable = true;
+    services.openssh.settings.PasswordAuthentication = false;
     users.users.root.openssh.authorizedKeys.keys = cfg.keys;
   };
 }
