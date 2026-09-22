086b5c370fc30652f4ddf33f95c41f375064b374
Lucas Desgouilles <lucas.desgouilles@numerique.gouv.fr>
2026-09-22 17:02:58 +0200

    fix: eval error introduced in #265

--- modules/graphical-interface/plasma.nix
+++ modules/graphical-interface/plasma.nix
@@ -36,7 +36,9 @@ in
     services.displayManager.sddm = {
       enable = true;
       settings = mkIf config.securix.admins.enable {
-        Users.HideUsers = lib.concatMapStringsSep "," (admin: admin.name) config.securix.admins.accounts;
+        Users.HideUsers = lib.concatMapAttrsStringsSep "," (
+          _: admin: admin.name
+        ) config.securix.admins.accounts;
       };
     };
 

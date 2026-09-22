cloud-gouv/securix

commit 514a59ebeff7325685b4dfd13d1bd60384541eb0

feat: hide admins in SDDM (#265)
The admins we add show as users in the login screen, it is kind of
distracting to users.
All admins should know how to log in through the "Other user" option
or directly through a tty.

modules/graphical-interface/plasma.nix
@@ -32,7 +32,14 @@ in
   };
   config = mkIf (cfg.variant == "kde") {
     services.xserver.enable = true;
-    services.displayManager.sddm.enable = true;
+
+    services.displayManager.sddm = {
+      enable = true;
+      settings = mkIf config.securix.admins.enable {
+        Users.HideUsers = lib.concatMapStringsSep "," (admin: admin.name) config.securix.admins.accounts;
+      };
+    };
+
     services.desktopManager.plasma6.enable = true;
 
     environment.systemPackages = cfg.kde.lookAndFeelPackages;

commit 086b5c370fc30652f4ddf33f95c41f375064b374

fix: eval error introduced in #265

modules/graphical-interface/plasma.nix
@@ -36,7 +36,9 @@ in
     services.displayManager.sddm = {
       enable = true;
       settings = mkIf config.securix.admins.enable {
-        Users.HideUsers = lib.concatMapStringsSep "," (admin: admin.name) config.securix.admins.accounts;
+        Users.HideUsers = lib.concatMapAttrsStringsSep "," (
+          _: admin: admin.name
+        ) config.securix.admins.accounts;
       };
     };
 