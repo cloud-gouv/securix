cloud-gouv/securix

commit 22a78cc2a8b75ec3463a28caab7cd1d00c69366b

fix: eval error introduced in #265 (#269)
* fix: eval error introduced in #265

* tests/minimal: include the KDE flavor instead of sway

We more commonly deploy the KDE flavor, it's better if errors around it
are caught early

modules/graphical-interface/plasma.nix
@@ -36,7 +36,9 @@ in
     services.displayManager.sddm = {
       enable = true;
       settings = mkIf config.securix.admins.enable {
-        Users.HideUsers = lib.concatMapStringsSep "," (admin: admin.name) config.securix.admins.accounts;
+        Users.HideUsers = lib.concatMapAttrsStringsSep "," (
+          _: admin: admin.name
+        ) config.securix.admins.accounts;
       };
     };
 

tests/minimal.nix
@@ -11,7 +11,7 @@ let
     modules = [
       {
         securix = {
-          graphical-interface.variant = "sway";
+          graphical-interface.variant = "kde";
           self = {
             mainDisk = "/dev/nvme0n1";
             machine = {

commit 96a27763afbc6833841f39de55fd583a3b0eceb5

fix: eval error introduced in #269...

modules/graphical-interface/plasma.nix
@@ -36,7 +36,7 @@ in
     services.displayManager.sddm = {
       enable = true;
       settings = mkIf config.securix.admins.enable {
-        Users.HideUsers = lib.concatMapAttrsStringsSep "," (
+        Users.HideUsers = lib.concatMapAttrsStringSep "," (
           _: admin: admin.name
         ) config.securix.admins.accounts;
       };